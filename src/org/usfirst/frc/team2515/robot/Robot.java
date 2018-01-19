5c8e76e7fcd15c6ac2d5a0f76b4b35bf36c5b39f
Kelly Schuerman <Kelly.Schuerman@smsu.edu>
2018-01-19 14:30:38 -0600

    robot - added vision mat, expected rectangle displayed on the video stream

--- src/org/usfirst/frc/team2515/robot/Robot.java
+++ src/org/usfirst/frc/team2515/robot/Robot.java
@@ -7,6 +7,14 @@
 
 package org.usfirst.frc.team2515.robot;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -34,6 +42,8 @@ public class Robot extends IterativeRobot {
 	private AnalogGyro m_gyro = new AnalogGyro(kGyroPort);
 	private Joystick m_stick;
 
+	Thread m_visionThread;
+	
 	@Override
 	public void robotInit() {
 		Spark frontLeft = new Spark(kFrontLeftChannel);
@@ -52,8 +62,40 @@ public class Robot extends IterativeRobot {
 
 		m_gyro.setSensitivity(kVoltsPerDegreePerSecond);
 
-		CameraServer.getInstance().startAutomaticCapture();
+		m_visionThread = new Thread(() -> {
+			// Get the UsbCamera from CameraServer
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+			// Set the resolution
+			camera.setResolution(640, 480);
 
+			// Get a CvSink. This will capture Mats from the camera
+			CvSink cvSink = CameraServer.getInstance().getVideo();
+			// Setup a CvSource. This will send images back to the Dashboard
+			CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+
+			// Mats are very memory expensive. Lets reuse this Mat.
+			Mat mat = new Mat();
+
+			// This cannot be 'true'. The program will never exit if it is. This
+			// lets the robot stop this thread when restarting robot code or
+			// deploying.
+			while (!Thread.interrupted()) {
+				// Tell the CvSink to grab a frame from the camera and put it
+				// in the source mat.  If there is an error notify the output.
+				if (cvSink.grabFrame(mat) == 0) {
+					// Send the output the error.
+					outputStream.notifyError(cvSink.getError());
+					// skip the rest of the current iteration
+					continue;
+				}
+				// Put a rectangle on the image
+				Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+				// Give the output stream a new image to display
+				outputStream.putFrame(mat);
+			}
+		});
+		m_visionThread.setDaemon(true);
+		m_visionThread.start();
 	}
 
 	@Override
